70f749fba3894815023e8db798c561e3f19c3819
Jerry Wang <tianchenwang@ucsb.edu>
2022-01-31 19:08:51 -0800

    implement query_generator function

--- query_generator.py
+++ query_generator.py
@@ -1,6 +1,19 @@
 from mimetypes import init
 from time import clock_gettime
+import random
 
-def generate(page_num):
+# there are "page_num" distinct pages
+PAGE_NUM = 100
+# our query requests "query_length" pages
+QUERY_LENGTH = 100
+
+def generate(page_num, query_length):
     output = []
+    for i in range(query_length):
+        output.append(random.randint(1, page_num))
+
     return output
+
+if __name__ == "__main__":
+    query = generate(PAGE_NUM, QUERY_LENGTH)
+    print(query)
