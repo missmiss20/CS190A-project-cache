bb1c56879a9b7b7e780045eeae009684b900e37f
Jerry Wang <tianchenwang@ucsb.edu>
2022-01-31 18:53:47 -0800

    implement set function and request generator in cache.py

--- cache.py
+++ cache.py
@@ -1,25 +1,42 @@
 from mimetypes import init
 from time import clock_gettime
+import random
 
 class cache:
-    def __init__(self, page_requests, cache_size = 100, method="FIFO"):  
-        self.PAGE_REQUESTS = page_requests
-        self.CACHE_SIZE = cache_size
-        self.CACHE_METHOD = method
+    def __init__(self, total_page_num, page_request_count, cache_size = 100):
+        self.total_page_num = total_page_num
+        self.page_request_count = page_request_count
+        self.cache_size = cache_size
+        self.requests = self.generate_requests(total_page_num, page_request_count)
 
 
-    def start_caching(self):
-        if self.CACHE_METHOD == "FIFO":
+    def reset_test_values(self, total_page_num, page_request_count, cache_size):
+        self.total_page_num = total_page_num
+        self.page_request_count = page_request_count
+        self.cache_size = cache_size        
+
+    def generate_requests(self):
+        output = []
+        for _ in range(self.page_request_count):
+            output.append(random.randint(1, self.total_page_num))
+
+        self.requests = output
+
+
+    def start_caching(self, method):
+        if method == "FIFO":
             self.FIFO()
-        elif self.CACHE_METHOD == "LIFO":
+        elif method == "LIFO":
             self.LIFO()
-        elif self.CACHE_METHOD == "LRU":
+        elif method == "LRU":
             self.LRU()
-        elif self.CACHE_METHOD == "LFU":
+        elif method == "LFU":
             self.LFU()
+        elif method == "LFD":
+            self.LFD()
+        else:
+            print("Error: Please input a valid caching method")
 
-        
-        self.LFD()
         return 0
 
 
@@ -37,6 +54,13 @@ class cache:
     
     def LFD(self):
         return 0
+
+
+    def set_cache_size(self, cache_size):
+        self.CACHE_SIZE = cache_size
+
+    def set_request_page_count(self, page_request_count):
+        self.PAGE_REQUESTS_COUNT = page_request_count
 
 
 
